e50eabfab7e505189f758ce9ecb369dc39ed1262
domicat <shui_mu98@163.com>
2022-04-01 20:18:03 +0800

    delete xlsx2lua

--- xlsx2lua/build.bat
+++
@@ -1,5 +0,0 @@
-echo off
-
-go build -ldflags "-w -s" -o ../../../config/xlsx2lua.exe ./xlsx2lua.go
-.\upx ../../../config/xlsx2lua.exe
-pause
--- xlsx2lua/xlsx2lua.bat
+++
@@ -1,4 +0,0 @@
-echo off
-rem rd /s/q %cd%\l-xlsx
-xlsx2lua.exe -i xlsx -o l-xlsx
-pause
